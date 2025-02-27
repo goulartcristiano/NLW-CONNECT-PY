b85631e9f7acb0306fb70db20ed3897a528341a5
goulartcristiano <cristianovgoulart@gmail.com>
2025-02-27 14:16:26 -0300

    feat: Implemeting connection in DB and adding repository

---
+++ src/model/configs/connection.py
@@ -0,0 +1,21 @@
+# GERENTE DE CONECXÃO
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+class DBConnectionHandler:
+    def __init__(self):
+        self.__connection_string = "sqlite:///schema.db" #MOSTRA AO SQLITE ONDE O BANCO DE DADOS ESTÁ
+        self.__engine = self.__create_database_engine()
+        self.session = None
+
+    def __create_database_engine(self):
+        engine = create_engine(self.__connection_string)
+        return engine
+    
+    def __enter__(self):
+        session_maker = sessionmaker(bind=self.__engine)
+        self.session = session_maker()
+        return self
+ 
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
